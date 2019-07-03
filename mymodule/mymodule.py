1228f02b214940acd287b9f6dd96409de27b6a19
Rohit Mistry <rohit.mistry@righteous.ai>
2019-07-03 01:36:58 -0400

    Root cause fixed after discovery

--- mymodule/mymodule.py
+++ mymodule/mymodule.py
@@ -1,5 +1,14 @@
 # Testing relative import with IPython Kernel in VS Code
 
+#%% Change working directory from the workspace root to the .py file location.
+import os
+try:
+    os.chdir(os.path.join(os.getcwd(), 'mymodule')) # change directory
+    print(os.getcwd())
+except:
+    pass # ignore error in-case re-running this cell
+    # (e.g. new cwd = `Project root/mymodule/mymodule`)
+
 #%%
 import os
 import sys
